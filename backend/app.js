b4ec0e395c4d4f1417dcd21df18ea0fc6087dbf0
mohammadm10 <mohammadali_10@hotmail.com>
2023-10-28 23:03:07 -0700

    db incorporated

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,54 @@
+require('dotenv').config();
+
 const express = require('express');
-const cors = require('cors'); 
+const mysql = require('mysql2');
+const cors = require('cors');
 const app = express();
 const port = 5000;
 
 app.use(express.json());
-app.use(cors()); 
+app.use(cors());
+
+const connection = mysql.createConnection({
+  host: process.env.DATABASE_HOST,     // MySQL host
+  user: process.env.DATABASE_USER,     // MySQL username
+  password: process.env.DATABASE_PASSWORD, // MySQL password
+  database: process.env.DATABASE_DB, // Name of your database
+  port: process.env.DATABASE_PORT,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database: ' + err.stack);
+    return;
+  }
+  console.log('Connected to the database as id ' + connection.threadId);
+});
+
+app.post('/createUser', (req, res) => {
+  const { username, password } = req.body;
+
+  const query = `INSERT INTO users 
+  (username, password)
+  VALUES 
+  (?, ?)`
+  const values = [username, password];
+  connection.query(query, values, (err, results) => {
+    if (err) {
+      console.error('Error executing the query: ' + err.stack);
+      return;
+    }
+    console.log('Inserted new user into the database');
+    res.send('Login successful');
+  });
+})
+
 
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
   console.log('Received username:', username);
   console.log('Received password:', password);
-  res.send('Login successful');
+
 });
 
 app.get('/', (req, res) => {
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,60 @@
-export default function HomePage () {
+import Head from 'next/head';
+import { Button, TextField, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TablePagination } from '@mui/material';
+
+export default function HomePage() {
+
+    const data = [
+        { item: 'Item 1', amount: 100 },
+        { item: 'Item 2', amount: 200 },
+        { item: 'Item 3', amount: 150 },
+    ];
+
     return (
         <>
-         Hello
+            <Head>
+                <title>Finanace Tracker</title>
+            </Head>
+            <main id='loggedIn' className='bg-white overflow-hidden'>
+                <section className='min-h-screen flex justify-center items-center flex-col space-y-4' >
+                    <div className=' flex justify-between w-[25%]'>
+                        <TextField id="item" label="Item" variant="outlined" />
+                        <TextField id="amount" label="Amount" variant="outlined" />
+                    </div>
+                    <div className=''>
+                        <Button
+                            style={{
+                                borderRadius: 17,
+                                padding: "10px 30px",
+                                fontSize: "12px",
+                                backgroundColor: "#F64C72",
+                                color: "#FFF"
+                            }}
+                        >
+                            Enter
+                        </Button>
+                    </div>
+                    <div className=' w-[50%]'>
+                        <TableContainer component={Paper}>
+                            <Table>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Item</TableCell>
+                                        <TableCell>Amount</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {data.map((row, index) => (
+                                        <TableRow key={index}>
+                                            <TableCell>{row.item}</TableCell>
+                                            <TableCell>{row.amount}</TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </div>
+                </section>
+            </main>
         </>
     )
 }
